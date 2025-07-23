73edee15582e935cabf830f12902b99573fbf2a7
IlonaYunosheva <yunoshevailona@gmail.com>
2025-07-23 14:47:50 +0200

    java-classes-and-objects-fields-and-methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+                SuperHero hero1 = new SuperHero("Флеш", 30, 5);
+                SuperHero hero2 = new SuperHero("Залізна Людина", 25, 10);
 
+                int round = 1;
+                while (hero1.isAlive() && hero2.isAlive()) {
+                    System.out.println("Раунд " + round + ":");
+                    hero1.attack(hero2);
+                    if (!hero2.isAlive()) break;
+
+                    hero2.attack(hero1);
+                    if (!hero1.isAlive()) break;
+
+                    round++;
+                    System.out.println();
+                }
+
+                if (hero1.isAlive() && !hero2.isAlive()) {
+                    System.out.println("Переможець: " + hero1.name + "!");
+                } else if (!hero1.isAlive() && hero2.isAlive()) {
+                    System.out.println("Переможець: " + hero2.name + "!");
+                } else {
+                    System.out.println("Нічия!");
+                }
+            }
+        }
         // Тут має розгортатися епічна битва між Супергероями
 
-    }
-}
--- src/SuperHero.java
+++ src/SuperHero.java
@@ -1,4 +1,34 @@
 public class SuperHero {
+        public String name;
+        public int attackPower;
+        public int defensePower;
+        public int health;
 
-    // Тут мають бути властивості і поведінка Супергероя
+    public SuperHero(String name, int attackPower, int defensePower) {
+        this.name = name;
+        this.attackPower = attackPower;
+        this.defensePower = defensePower;
+        this.health = 100;
+    }
+    public void attack(SuperHero opponent) {
+        int damage = this.attackPower - opponent.defensePower;
+        System.out.println(this.name + " атакує " + opponent.name + " і завдає " + damage + " ушкоджень.");
+        opponent.takeDamage(damage);
+    }
+
+    public void takeDamage(int damage) {
+        this.health -= damage;
+        System.out.println(this.name + " має залишок здоров'я: " + this.health + ".");
+    }
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+    public void showStats() {
+        System.out.println("Герой: " + name + ", Атака: " + attackPower + ", Захист: " + defensePower + ", Здоров'я: " + health);
+    }
 }
+
+        // Тут мають бути властивості і поведінка Супергероя
+
